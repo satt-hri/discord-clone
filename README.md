82e1a91db4135b8393cff21db97243ca6da6128e
satt-hri <h_ri@satt.jp>
2023-09-06 22:41:47 +0900

    Dark & Light Theme setup

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ auth
 https://dashboard.clerk.com/
 
   npm install @clerk/nextjs
+
+3.Dark & Light Theme setup
+    3.1
+      https://ui.shadcn.com/docs/dark-mode/next
+    npm i next-themes
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from "@/components/mode-toogle";
 import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle></ModeToggle>
     </div>
   );
 }
