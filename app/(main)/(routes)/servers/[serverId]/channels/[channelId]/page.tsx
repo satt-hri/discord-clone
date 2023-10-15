8288035bbf0e09176ac1b5ee9cdaaf336abf4ef5
satt-hri <h_ri@satt.jp>
2023-10-15 21:56:58 +0900

    chatheader

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,44 @@
-const ChannelIdPage = () => {
-    return (<>
-     Channel id page
-    </>);
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ChatHeader } from "@/components/chat/chat-header";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
 }
- 
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const memmber = await db.member.findFirst({
+    where: {
+      profileId: profile.id,
+      serverId: params.serverId,
+    },
+  });
+
+  if (!channel || !memmber) {
+    redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] h-full flex flex-col">
+      <ChatHeader serverId={channel.serverId} name={channel.name} type="channel" />
+    </div>
+  );
+};
+
 export default ChannelIdPage;
