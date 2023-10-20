ff26b4d60bb0dce050a373248986320b7c4827d1
satt-hri <h_ri@satt.jp>
2023-10-20 17:51:07 +0900

    emoji

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -2,13 +2,15 @@
 
 import { useForm } from "react-hook-form";
 import * as z from "zod";
-import qs from "query-string"
+import qs from "query-string";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormField, FormItem } from "../ui/form";
-import { Plus, Smile } from "lucide-react";
-import { Input } from "../ui/input";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Plus, } from "lucide-react";
+import { Input } from "@/components/ui/input";
 import axios from "axios";
 import { useModal } from "@/hooks/user-modal-store";
+import { EmojiPicker } from "@/components/emoji-picker";
+import { useRouter } from "next/navigation";
 
 interface ChatInputProps {
   name: string;
@@ -29,21 +31,19 @@ const ChatInput = ({ name, apiUrl, type, query }: ChatInputProps) => {
     },
   });
 
-  const {onOpen} = useModal()
+  const { onOpen } = useModal();
+  const router = useRouter()
 
   const isLoading = form.formState.isLoading;
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       const url = qs.stringifyUrl({
-        url:apiUrl,
-        query
-      })
-      await axios.post(url,values)
-
-    } catch (error) {
-      
-    }
-    
+        url: apiUrl,
+        query,
+      });
+      await axios.post(url, values);
+      form.reset();
+    } catch (error) {}
   };
   return (
     <Form {...form}>
@@ -57,7 +57,9 @@ const ChatInput = ({ name, apiUrl, type, query }: ChatInputProps) => {
                 <div className="relative p-4 pb-6">
                   <button
                     type="button"
-                    onClick={() => {onOpen("messageFile",{apiUrl,query})}}
+                    onClick={() => {
+                      onOpen("messageFile", { apiUrl, query });
+                    }}
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover: bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark-text-[#313338]"></Plus>
@@ -65,10 +67,18 @@ const ChatInput = ({ name, apiUrl, type, query }: ChatInputProps) => {
                   <Input
                     disabled={isLoading}
                     className="py-6 px-14 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
-                    placeholder={`Message ${type === "conversation" ? name:"#" + name}`}
+                    placeholder={`Message ${
+                      type === "conversation" ? name : "#" + name
+                    }`}
                     {...field}
                   ></Input>
-                  <div className="absolute top-7 right-8"><Smile></Smile></div>
+                  <div className="absolute top-7 right-8">
+                    <EmojiPicker
+                      onChange={(emoji: string) =>
+                        field.onChange(`${field.value} ${emoji}`)
+                      }
+                    />
+                  </div>
                 </div>
               </FormControl>
             </FormItem>
