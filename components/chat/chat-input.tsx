abedd71b60689fe82bde6446d4c4e9263cd4e52f
h_ri <h_ri@satt.jp>
2024-04-02 18:19:07 +0900

    ts error

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -60,7 +60,7 @@ const ChatInput = ({ name, apiUrl, type, query }: ChatInputProps) => {
                     onClick={() => {
                       onOpen("messageFile", { apiUrl, query });
                     }}
-                    className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover: bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
+                    className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark-text-[#313338]"></Plus>
                   </button>
